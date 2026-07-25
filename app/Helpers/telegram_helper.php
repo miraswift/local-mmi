cc2fac3af6f92c18b7eaa23c5b8eec149ed95156
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2026-07-25 11:51:52 +0700

    Notif tele penimbangan

--- app/Helpers/telegram_helper.php
+++ app/Helpers/telegram_helper.php
@@ -1,39 +1,34 @@
 <?php
 
-namespace App\Helpers;
-
-class telegram_helper
+function sendMessageTelegram($message)
 {
-    public function sendMessageTelegram($message)
-    {
-        $curl = curl_init();
+    $curl = curl_init();
 
-        $chatId =  -1004393854603;
+    $chatId =  -1004393854603;
 
-        $telegramPayload = [
-            'chat_id' => $chatId,
-            'text' => $message,
-        ];
+    $telegramPayload = [
+        'chat_id' => $chatId,
+        'text' => $message,
+    ];
 
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://api.telegram.org/bot8494834740:AAEZLYfkzUhrY6GroazEJOf876oToo2-qIw/sendMessage',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => json_encode($telegramPayload),
-            CURLOPT_HTTPHEADER => array(
-                'Content-Type: application/json'
-            ),
-        ));
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://api.telegram.org/bot8494834740:AAEZLYfkzUhrY6GroazEJOf876oToo2-qIw/sendMessage',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => json_encode($telegramPayload),
+        CURLOPT_HTTPHEADER => array(
+            'Content-Type: application/json'
+        ),
+    ));
 
-        $response = curl_exec($curl);
+    $response = curl_exec($curl);
 
-        curl_close($curl);
+    curl_close($curl);
 
-        return json_decode($response, true);
-    }
+    return json_decode($response, true);
 }
