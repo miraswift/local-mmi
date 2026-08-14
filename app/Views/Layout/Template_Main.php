c0722a4bacde068736341a0515ae7778e6a19d41
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2026-08-15 01:09:39 +0700

    Report by hour

--- app/Views/Layout/Template_Main.php
+++ app/Views/Layout/Template_Main.php
@@ -165,12 +165,12 @@
                                         <p>Report</p>
                                     </a>
                                 </li>
-                                <!-- <li class="nav-item">
+                                <li class="nav-item">
                                     <a href="/reportbyhour" class="nav-link <?= $menu == 'ReportByHour' ? 'active' : '' ?>">
                                         <i class="fas fa-clock nav-icon"></i>
                                         <p>Batch By Hour</p>
                                     </a>
-                                </li> -->
+                                </li>
                             </ul>
                         </li>
                     </ul>
